872e5892acc58d3eb9e309881289b3355f193644
Nicodry20 <48178309@est.ort.edu.ar>
2024-03-13 10:22:46 -0300

    Ej12

---
+++ Guia de programacion 1/Assets/Ej12.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej12 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int dado1 = Random.Range(1,7);
+        int dado2 = Random.Range(1,7);
+        int dado3 = Random.Range(1,7);
+
+        Debug.Log("Dado1: " + dado1);
+        Debug.Log("Dado2: " + dado2);
+        Debug.Log("Dado3: " + dado3);
+
+        if (dado1 == 6 && dado2 == 6 && dado3 == 6)
+        {
+            Debug.Log("Excelente");
+        }
+        else if ((dado1 == 6 && dado2 == 6) || (dado1 == 6 && dado3 == 6 || dado2 == 6 && dado3 ==6))
+        {
+            Debug.Log("Muy bien");
+        }
+        else if (dado1 == 6 || dado2 == 6 || dado3 == 6)
+        {
+            Debug.Log("Regular");
+        }
+        else
+        {
+            Debug.Log("Insuficiente");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
