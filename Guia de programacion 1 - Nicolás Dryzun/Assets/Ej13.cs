826e92ba329350826c7f7c64e22f9b7c7bc83074
Nicodry20 <48178309@est.ort.edu.ar>
2024-03-14 11:51:03 -0300

    Ej14

--- Guia de programacion 1 - Nicolás Dryzun/Assets/Ej13.cs
+++ Guia de programacion 1 - Nicolás Dryzun/Assets/Ej13.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Ej13 : MonoBehaviour
 {
+    float SALARIO_HORA_NORMAL = 16f;
+    float SALARIO_HORA_EXTRA = 20f;
+    public int horasTrabajadas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float salarioSemanal;
+
+        if (horasTrabajadas <= 40)
+        {
+            salarioSemanal = horasTrabajadas * SALARIO_HORA_NORMAL;
+        }
+        else
+        {
+            int horasNormales = 40;
+            int horasExtras = horasTrabajadas - horasNormales;
+            salarioSemanal = horasNormales * SALARIO_HORA_NORMAL + horasExtras * SALARIO_HORA_EXTRA;
+        }
+
+        Debug.Log("El salario semanal del obrero es: $" + salarioSemanal);
     }
 
     // Update is called once per frame
---
+++ Guia de programacion 1 - Nicolás Dryzun/Assets/Ej14.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej14 : MonoBehaviour
+{
+    public char simbolo;
+    public int numero1;
+    public int numero2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (simbolo == 's')
+        {
+            int suma = numero1 + numero2;
+            Debug.Log("La suma entre " + numero1 + " y " + numero2 + " es " + suma);
+        }
+        else if (simbolo == 'r')
+        {
+            int resta = numero1 - numero2;
+            Debug.Log("La resta entre " + numero1 + " y " + numero2 + " es " + resta);
+        }
+        else if (simbolo == 'p')
+        {
+            int producto = numero1 * numero2;
+            Debug.Log("El producto entre " + numero1 + " y " + numero2 + " es " + producto);
+        }
+        else if (simbolo == 'd')
+        {
+            if (numero2 != 0)
+            {
+                float division = (float)numero1 / numero2;
+                Debug.Log("La división es: " + division);
+            }
+            else
+            {
+                Debug.Log("No se puede dividir por cero.");
+            }
+        }
+        else
+        {
+            Debug.Log("Operación no válida.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
